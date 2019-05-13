8a94b210f7a867c4477336e4f2e759dd20787326
Patrick Lamoureux <plamoureux84@gmail.com>
2019-05-13 15:35:08 -0600

    edited the routes and controller and index

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,22 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
